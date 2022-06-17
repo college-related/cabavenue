a9b7344af2833cb1c5d59e97a7762f9078d7a22d
Salipa-Gurung <salipagurung@gmail.com>
2022-06-17 22:42:24 +0545

    [UI]: change ui for ride history page

--- lib/pages/ride_history.dart
+++ lib/pages/ride_history.dart
@@ -45,41 +45,175 @@ class _MyRidePageState extends State<MyRidePage> {
           borderRadius: BorderRadius.circular(4),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1547721064-da6cfb341d50"),),
               Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1547721064-da6cfb341d50"),)
-                ],
-              ),
-              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                      const Text('Driver Name', textAlign: TextAlign.left,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                  const Text('Gopal Prasad Shrestha', textAlign: TextAlign.left,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
                   Row(
-                    children: const [
-                      Icon(Icons.location_on_outlined, color: Colors.blue,),
-                      Text('Bagar', textAlign: TextAlign.left,),
+                    children: [
+                      Column(
+                        children: const [
+                          Icon(Icons.location_on_outlined, color: Colors.blue,),
+                          Icon(Icons.share_location_rounded,color: Colors.blue),
+                        ]
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Bagar',),
+                          Text('Chipledhunga', textAlign: TextAlign.left,),
+                        ]
+                      ),
                     ],
                   ),
-                  Row(
-                    children: const [
-                      Icon(Icons.share_location_rounded,color: Colors.blue),
-                      // Text('Driver Name', textAlign: TextAlign.left,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
-                      Text('Chipledhunga', textAlign: TextAlign.left,),
-                    ],
-                  ),
+                  const Text('Distance travelled: 4.5km',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Date: 19/06/2022',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Time: 2pm',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
                 ],
               ),
+              const Text('\$200', style: TextStyle(fontWeight: FontWeight.w600,fontSize: 20, color: Color.fromARGB(255, 244, 129, 36)), )
+            ],
+          ),
+        )
+      ),
+      Card(
+        elevation: 1,
+        shadowColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1547721064-da6cfb341d50"),),
               Column(
-                children: const [
-                  Text('\$350', textAlign: TextAlign.end,),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Gopal Prasad Shrestha', textAlign: TextAlign.left,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                  Row(
+                    children: [
+                      Column(
+                        children: const [
+                          Icon(Icons.location_on_outlined, color: Colors.blue,),
+                          Icon(Icons.share_location_rounded,color: Colors.blue),
+                        ]
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Naya Gaon, Pokhara',),
+                          Text('Lamachaur, Pokhara', textAlign: TextAlign.left,),
+                        ]
+                      ),
+                    ],
+                  ),
+                  const Text('Distance travelled: 4.5km',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Date: 19/06/2022',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Time: 2pm',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
                 ],
-              )
+              ),
+              const Text('\$350', style: TextStyle(fontWeight: FontWeight.w600,fontSize: 20, color: Color.fromARGB(255, 244, 129, 36)), )
+            ],
+          ),
+        )
+      ),
+       Card(
+        elevation: 1,
+        shadowColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1547721064-da6cfb341d50"),),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Gopal Prasad Shrestha', textAlign: TextAlign.left,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                  Row(
+                    children: [
+                      Column(
+                        children: const [
+                          Icon(Icons.location_on_outlined, color: Colors.blue,),
+                          Icon(Icons.share_location_rounded,color: Colors.blue),
+                        ]
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Naya Gaon, Pokhara',),
+                          Text('Lamachaur, Pokhara', textAlign: TextAlign.left,),
+                        ]
+                      ),
+                    ],
+                  ),
+                  const Text('Distance travelled: 4.5km',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Date: 19/06/2022',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Time: 2pm',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                ],
+              ),
+              const Text('\$250', style: TextStyle(fontWeight: FontWeight.w600,fontSize: 20, color: Color.fromARGB(255, 244, 129, 36)), )
+            ],
+          ),
+        )
+      ),
+      Card(
+        elevation: 1,
+        shadowColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(backgroundImage: NetworkImage("https://images.unsplash.com/photo-1547721064-da6cfb341d50"),),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Gopal Prasad Shrestha', textAlign: TextAlign.left,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500)),
+                  Row(
+                    children: [
+                      Column(
+                        children: const [
+                          Icon(Icons.location_on_outlined, color: Colors.blue,),
+                          Icon(Icons.share_location_rounded,color: Colors.blue),
+                        ]
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Bagar',),
+                          Text('Chipledhunga', textAlign: TextAlign.left,),
+                        ]
+                      ),
+                    ],
+                  ),
+                  const Text('Distance travelled: 4.5km',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Date: 19/06/2022',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                  const Text('Time: 2pm',style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 127, 127, 127)),),
+                ],
+              ),
+              const Text('\$300', style: TextStyle(fontWeight: FontWeight.w600,fontSize: 20, color: Color.fromARGB(255, 244, 129, 36)), )
             ],
           ),
         )
@@ -104,22 +238,7 @@ class _MyRidePageState extends State<MyRidePage> {
       ),
     ],
   ),
-          //   Row(
-          //   children: [
-          //     Column(
-          //       children: const [
-          //         Text('Driver Name'),
-          //         Text('Source Location: Bagar'),
-          //         Text('Destination Location: Chipledhunga'),
-          //       ],
-          //     ),
-          //     Column(
-          //       children:const [
-          //         Text(r''),
-          //       ],
-          //     )
-          //   ],
-          // ),
+
     );
   }
 }
