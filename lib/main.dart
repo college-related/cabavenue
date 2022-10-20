61f2fbe08029730fe5d497a24f811b913e7ae268
Alson33 <garbuja12alson@gmail.com>
2022-10-20 17:32:50 +0545

    [fix]: fix widgets to new architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:cabavenue/pages/splash_screen.dart';
+import 'package:cabavenue/providers/profile_provider.dart';
+import 'package:cabavenue/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:cabavenue/pages/home.dart';
 import 'package:cabavenue/pages/auth.dart';
@@ -6,6 +8,7 @@ import 'package:cabavenue/pages/profile_edit.dart';
 import 'package:cabavenue/pages/ride_history.dart';
 import 'package:cabavenue/pages/emergency.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
@@ -17,21 +20,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProfileProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: AppTheme.main(),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/home': (context) => const HomePage(),
+          '/ride-history': (context) => const RideHistory(),
+          '/profile-edit': (context) => const EditProfilePage(),
+          '/emergency': (context) => const EmergencyPage(),
+          '/auth': (context) => const AuthPage(),
+        },
+        // home: const MyHomePage(),
       ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomePage(),
-        '/ride-history': (context) => const RideHistory(),
-        '/profile-edit': (context) => const EditProfilePage(),
-        '/emergency': (context) => const EmergencyPage(),
-        '/auth': (context) => const AuthPage(),
-      },
-      // home: const MyHomePage(),
     );
   }
 }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,6 +1,8 @@
+import 'package:cabavenue/helpers/validator.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({
     Key? key,
     required this.controller,
@@ -10,6 +12,13 @@ class CustomTextField extends StatelessWidget {
     this.iconColor = Colors.blue,
     this.borderType = 'full',
     this.isSecuredText = false,
+    this.colors = Colors.grey,
+    this.validations = const [],
+    this.length = 8,
+    this.hasError = false,
+    this.errorMessage = '',
+    this.focusNode,
+    this.onTap,
   }) : super(key: key);
 
   final TextEditingController controller;
@@ -19,29 +28,79 @@ class CustomTextField extends StatelessWidget {
   final Color iconColor;
   final String borderType;
   final bool isSecuredText;
+  final Color colors;
+  final List<String> validations;
+  final int length;
+  final bool hasError;
+  final String errorMessage;
+  final FocusNode? focusNode;
+  final Function? onTap;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool isHidden = true;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      keyboardType: keyboardType,
-      obscureText: isSecuredText,
-      decoration: InputDecoration(
-        icon: Icon(
-          icon,
-          color: iconColor,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        onTap: widget.onTap != null
+            ? () {
+                widget.onTap!();
+              }
+            : null,
+        focusNode: widget.focusNode,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        controller: widget.controller,
+        decoration: InputDecoration(
+          errorText: widget.hasError ? widget.errorMessage : null,
+          filled: true,
+          fillColor: const Color.fromARGB(47, 96, 125, 139),
+          isDense: true,
+          prefixIcon: Icon(
+            widget.icon,
+            color: widget.colors,
+          ),
+          suffixIcon: widget.isSecuredText
+              ? IconButton(
+                  icon: Icon(isHidden ? Iconsax.eye_slash : Iconsax.eye),
+                  onPressed: () {
+                    setState(() {
+                      isHidden = !isHidden;
+                    });
+                  },
+                )
+              : const SizedBox(height: 0, width: 0),
+          border: widget.borderType == 'full'
+              ? const OutlineInputBorder()
+              : const UnderlineInputBorder(),
+          labelText: widget.hintText,
+          labelStyle: const TextStyle(
+            color: Colors.grey,
+            fontSize: 14.0,
+          ),
         ),
-        border: borderType == 'full'
-            ? const OutlineInputBorder()
-            : const UnderlineInputBorder(),
-        labelText: hintText,
+        obscureText: (isHidden && widget.isSecuredText),
+        keyboardType: widget.keyboardType,
+        validator: (val) {
+          if (val == null || val.isEmpty) {
+            return 'Enter your ${widget.hintText}';
+          }
+          var validation = validator(
+            validations: widget.validations,
+            value: val,
+            length: widget.length,
+          );
+          if (!validation['isValidate']) {
+            return validation['message'];
+          }
+          return null;
+        },
       ),
-      validator: (val) {
-        if (val == null || val.isEmpty) {
-          return 'Enter your $hintText';
-        }
-        return null;
-      },
     );
   }
 }
