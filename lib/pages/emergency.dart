9e9e2221f3f86c125abaf05f30440e3459cc28e3
Alson33 <garbuja12alson@gmail.com>
2022-10-28 21:03:06 +0545

    [feature]: emergency call features

--- lib/pages/emergency.dart
+++ lib/pages/emergency.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:iconsax/iconsax.dart';
 
 class Emergency extends StatelessWidget {
@@ -20,8 +21,9 @@ class EmergencyPage extends StatefulWidget {
 class _EmergencyPageState extends State<EmergencyPage> {
   @override
   Widget build(BuildContext context) {
-    return   DefaultTabController(
-    length: 2,      child: Scaffold(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         appBar: AppBar(
           title: const Text('Emergency Services'),
           elevation: 2,
@@ -33,71 +35,115 @@ class _EmergencyPageState extends State<EmergencyPage> {
           ),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           child: TabBarView(
-            children:[
+            children: [
               SizedBox(
                 width: double.infinity,
                 height: MediaQuery.of(context).size.height * 0.5,
                 child: ListView(
-              children: [
-                ListTile(
-                  leading: const Icon(Icons.emergency),
-                  title: const Text('Ambulance'),
-                  isThreeLine: true,
-                  subtitle: const Text('102'),
-                  trailing: const Icon(Iconsax.call),
-                  tileColor: Colors.blue[100],
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
-                  horizontalTitleGap: 0,
-                  minVerticalPadding: 0,
-                  
-                ), 
-                ListTile(
-                  leading: Icon(Icons.emergency),
-                  title: Text('Police'),
-                  isThreeLine: true,
-                  subtitle: Text('100'),
-                  trailing: Icon(Iconsax.call),
-                ), 
-                ListTile(
-                  leading: Icon(Icons.emergency),
-                  title: Text('Fire Brigade'),
-                  isThreeLine: true,
-                  subtitle: Text('101'),
-                  trailing: Icon(Iconsax.call),
-                ), 
-                ListTile(
-                  leading: Icon(Icons.emergency),
-                  title: Text('Child help line'),
-                  isThreeLine: true,
-                  subtitle: Text('104'),
-                  trailing: Icon(Iconsax.call),
-                ), 
-                ListTile(
-                  leading: Icon(Icons.emergency),
-                  title: Text('Tourist Police'),
-                  isThreeLine: true,
-                  subtitle: Text('1144'),
-                  trailing: Icon(Iconsax.call),
-                ), 
-              ],
-            ),
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.emergency),
+                      title: const Text('Ambulance'),
+                      subtitle: const Text('102'),
+                      trailing: const Icon(Iconsax.call),
+                      tileColor: Colors.blue[100],
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10.0),
+                      horizontalTitleGap: 0,
+                      minVerticalPadding: 0,
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('102');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Iconsax.alarm),
+                      title: const Text('Police'),
+                      subtitle: const Text('100'),
+                      trailing: const Icon(Iconsax.call),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10.0),
+                      horizontalTitleGap: 0,
+                      minVerticalPadding: 0,
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('100');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Iconsax.radar_1),
+                      title: const Text('Fire Brigade'),
+                      subtitle: const Text('101'),
+                      trailing: const Icon(Iconsax.call),
+                      tileColor: Colors.blue[100],
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10.0),
+                      horizontalTitleGap: 0,
+                      minVerticalPadding: 0,
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('101');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Iconsax.security_user),
+                      title: const Text('Child help line'),
+                      subtitle: const Text('104'),
+                      trailing: const Icon(Iconsax.call),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10.0),
+                      horizontalTitleGap: 0,
+                      minVerticalPadding: 0,
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('104');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Iconsax.security_safe),
+                      title: const Text('Tourist Police'),
+                      subtitle: const Text('1144'),
+                      trailing: const Icon(Iconsax.call),
+                      tileColor: Colors.blue[100],
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10.0),
+                      horizontalTitleGap: 0,
+                      minVerticalPadding: 0,
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('1144');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.emergency),
+                      title: const Text('Test'),
+                      subtitle: const Text('982514080*'),
+                      trailing: const Icon(Iconsax.call),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10.0),
+                      horizontalTitleGap: 0,
+                      minVerticalPadding: 0,
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('9825140802');
+                      },
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 width: double.infinity,
                 height: MediaQuery.of(context).size.height * 0.5,
                 child: ListView(
-              children: const [
-                ListTile(
-                  leading: Icon(Iconsax.car),
-                  title: Text('Cars'),
-                  isThreeLine: true,
-                  subtitle: Text('9876543210'),
-                  trailing: Icon(Iconsax.call),
-                ),  
-              ],
-            ),
+                  children: [
+                    ListTile(
+                      leading: const Icon(Iconsax.car),
+                      title: const Text('Ram bahadur'),
+                      isThreeLine: true,
+                      subtitle: const Text('9876543210'),
+                      trailing: const Icon(Iconsax.call),
+                      onTap: () async {
+                        await FlutterPhoneDirectCaller.callNumber('9876543210');
+                      },
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
