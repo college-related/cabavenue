8a3addd6f12bbe30dc866e26d94968743244b3fa
Alson33 <garbuja12alson@gmail.com>
2022-10-29 13:17:18 +0545

    [test-1]: icon list

--- lib/pages/favorite_places.dart
+++ lib/pages/favorite_places.dart
@@ -41,92 +41,107 @@ class _FavoritePlacesState extends State<FavoritePlaces> {
         title: const Text('Favourite Places'),
         elevation: 2,
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25)),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 50.0,
+                        ),
                       ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 12,
-                        horizontal: 50.0,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text(
+                        "Add new place",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.white),
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text(
-                      "Add new place",
-                      style: TextStyle(
-                          fontSize: 14,
-                          letterSpacing: 2.2,
-                          color: Colors.white),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Text(
-                'Your favorite places',
-                style: Theme.of(context).textTheme.displaySmall,
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Text(
+                  'Your favorite places',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
               ),
-            ),
-            FutureBuilder<List<FavoriteModel>>(
-              future: favoritePlaces,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
+              FutureBuilder<List<FavoriteModel>>(
+                future: favoritePlaces,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return SizedBox(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: IconList()
+                                .getList[snapshot.data![index].iconIndex],
+                            title: Text(snapshot.data![index].givenName),
+                            subtitle: Text(
+                                '${snapshot.data![index].latitude}, ${snapshot.data![index].longitude}'),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Iconsax.edit),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Iconsax.trash),
+                                ),
+                              ],
+                            ),
+                            tileColor: index % 2 != 0
+                                ? Colors.white
+                                : Colors.teal[100],
+                          );
+                        },
+                      ),
+                    );
+                  }
                   return SizedBox(
                     width: double.infinity,
                     height: MediaQuery.of(context).size.height * 0.6,
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: IconList()
-                              .getList[snapshot.data![index].iconIndex],
-                          title: Text(snapshot.data![index].givenName),
-                          subtitle: Text(
-                              '${snapshot.data![index].latitude}, ${snapshot.data![index].longitude}'),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Iconsax.edit),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Iconsax.trash),
-                              ),
-                            ],
-                          ),
-                          tileColor:
-                              index % 2 != 0 ? Colors.white : Colors.teal[100],
-                        );
-                      },
-                    ),
+                    child: const Center(child: CircularProgressIndicator()),
                   );
-                }
-                return SizedBox(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  child: const Center(child: CircularProgressIndicator()),
-                );
-              },
-            ),
-          ],
+                },
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 50,
+                child: ListView.builder(
+                  itemCount: IconList().getList.length,
+                  itemBuilder: (contex, index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: IconList().getList[index],
+                  ),
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
